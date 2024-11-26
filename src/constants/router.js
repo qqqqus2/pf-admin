a8e2e4e93133d17097e05d19532dc22c51013dd6
Sangwoo Byun <bsw@cntt.co.kr>
2024-11-26 18:19:53 +0900

    라우터 수정

--- src/constants/router.js
+++ src/constants/router.js
@@ -1,95 +1,95 @@
 export const pubRouter = [
-  // 로그인
-  {
-    path: "/pub/page/login01",
-    title: "_로그인",
-  },
-  {
-    path: "/pub/page/login02",
-    title: "_로그인",
-  },
-  {
-    path: "/pub/page/login03",
-    title: "_로그인>인증번호입력",
-  },
-  // 01 사용자관리
-  {
-    path: "/pub/page/list01",
-    title: "_사용자관리>회원관리_목록_(ID BO021001)",
-  },
-  {
-    path: "/pub/page/detail01",
-    title: "_사용자관리>회원관리",
-  },
-  {
-    path: "/pub/page/detail02a",
-    title: "_파트너/시설관리>파트너관리>계약등록_(ID BO031002)",
-  },
-  {
-    path: "/pub/page/detail02b",
-    title: "_파트너/시설관리>파트너관리>계약상세>계약조회_(ID BO031003)",
-  },
+    // 로그인
+    {
+        path: "/pub/page/login01",
+        title: "_로그인",
+    },
+    {
+        path: "/pub/page/login02",
+        title: "_로그인",
+    },
+    {
+        path: "/pub/page/login03",
+        title: "_로그인>인증번호입력",
+    },
+    // 01 사용자관리
+    {
+        path: "/pub/page/list01",
+        title: "_사용자관리>회원관리_목록_(ID BO021001)",
+    },
+    {
+        path: "/pub/page/detail01",
+        title: "_사용자관리>회원관리",
+    },
+    {
+        path: "/pub/page/detail02a",
+        title: "_파트너/시설관리>파트너관리>계약등록_(ID BO031002)",
+    },
+    {
+        path: "/pub/page/detail02b",
+        title: "_파트너/시설관리>파트너관리>계약상세>계약조회_(ID BO031003)",
+    },
 
-  // 04 쿠폰/포인트관리
-  {
-    path: "/pub/page/couponMgt/list",
-    title: "_쿠폰/포인트관리>쿠폰관리>쿠폰리스트(BO051001) - hiuxc작업",
-  },
+    // 04 쿠폰/포인트관리
+    {
+        path: "/pub/page/couponMgt/list",
+        title: "_쿠폰/포인트관리>쿠폰관리>쿠폰리스트(BO051001) - ( hiuxc 작업 )",
+    },
 
-  {
-    path: "/pub/page/pointMgt/list",
-    title: "_쿠폰/포인트관리>포인트관리>포인트리스트(BO052001) - hiuxc작업",
-  },
-  {
-    path: "/pub/page/voucherMgt/list",
-    title: "_쿠폰/포인트관리>포인트상품권관리>상품권리스트(BO053001) - hiuxc작업",
-  },
+    {
+        path: "/pub/page/pointMgt/list",
+        title: "_쿠폰/포인트관리>포인트관리>포인트리스트(BO052001) - ( hiuxc 작업 )",
+    },
+    {
+        path: "/pub/page/voucherMgt/list",
+        title: "_쿠폰/포인트관리>포인트상품권관리>상품권리스트(BO053001) - ( hiuxc 작업 )",
+    },
 
-  // 05 시설관리 (tab)
-  {
-    path: "/pub/page/facilityMgt/list",
-    title: "_시설관리>AIot 센서 관리>리스트(탭구조) - hiuxc작업",
-  },
+    // 05 시설관리 (tab)
+    {
+        path: "/pub/page/facilityMgt/list",
+        title: "_시설관리>파트너/시설관리>리스트(==== 탭 구조 ====) - ( hiuxc 작업 )",
+    },
 
-  // 06 문의관리
-  {
-    path: "/pub/page/qnaMgt/list",
-    title: "_문의관리>공유신청관리>리스트(BO061001) - hiuxc작업",
-  },
-  {
-    path: "/pub/page/qnaMgt/regist",
-    title: "_문의관리>공유신청관리>조회/상세(BO061002) - hiuxc작업",
-  },
-  {
-    path: "/pub/page/oneononeMgt/list",
-    title: "_문의관리>1:1문의관리>리스트(BO062001) - hiuxc작업",
-  },
-  {
-    path: "/pub/page/oneononeMgt/regist",
-    title: "_문의관리>1:1문의관리>조회/상세(BO062002){답변 전} - hiuxc작업",
-  },
-  {
-    path: "/pub/page/oneononeMgt/regist?case1=true",
-    title: "_문의관리>1:1문의관리>조회/상세(BO062003){답변 후} - hiuxc작업",
-  },
-  {
-    path: "/pub/page/reportMgt/list",
-    title: "_문의관리>제보관리>리스트(BO064001) - hiuxc작업",
-  },
-  {
-    path: "/pub/page/reportMgt/regist",
-    title: "_문의관리>제보관리>조회/상세(BO064002) - hiuxc작업",
-  },
-  {
-    path: "/pub/page/paymentMgt/list",
-    title: "_문의관리>결제대행관리>리스트(BO065001) - hiuxc작업",
-  },
-  {
-    path: "/pub/page/paymentMgt/list?case1=true",
-    title: "_문의관리>결제대행관리>리스트(BO065001){회원 + B2G(시간권)} - hiuxc작업",
-  },
-  {
-    path: "/pub/page/paymentMgt/list?case2=true",
-    title: "_문의관리>결제대행관리>리스트(BO065001){비회원 + B2B / B2C} - hiuxc작업",
-  },
+    // 06 문의관리
+    {
+        path: "/pub/page/qnaMgt/list",
+        title: "_문의관리>공유신청관리>리스트(BO061001) - ( hiuxc 작업 )",
+    },
+    {
+        path: "/pub/page/qnaMgt/regist",
+        title: "_문의관리>공유신청관리>조회/상세(BO061002) - ( hiuxc 작업 )",
+    },
+    {
+        path: "/pub/page/oneononeMgt/list",
+        title: "_문의관리>1:1문의관리>리스트(BO062001) - ( hiuxc 작업 )",
+    },
+    {
+        path: "/pub/page/oneononeMgt/regist",
+        title: "_문의관리>1:1문의관리>조회/상세(BO062002){답변 전} - ( hiuxc 작업 )",
+    },
+    {
+        path: "/pub/page/oneononeMgt/regist?case1=true",
+        title: "_문의관리>1:1문의관리>조회/상세(BO062003){답변 후} - ( hiuxc 작업 )",
+    },
+    {
+        path: "/pub/page/reportMgt/list",
+        title: "_문의관리>제보관리>리스트(BO064001) - ( hiuxc 작업 )",
+    },
+    {
+        path: "/pub/page/reportMgt/regist",
+        title: "_문의관리>제보관리>조회/상세(BO064002) - ( hiuxc 작업 )",
+    },
+    {
+        path: "/pub/page/paymentMgt/list",
+        title: "_문의관리>결제대행관리>리스트(BO065001) - ( hiuxc 작업 )",
+    },
+    {
+        path: "/pub/page/paymentMgt/list?case1=true",
+        title: "_문의관리>결제대행관리>리스트(BO065001){회원 + B2G(시간권)} - ( hiuxc 작업 )",
+    },
+    {
+        path: "/pub/page/paymentMgt/list?case2=true",
+        title: "_문의관리>결제대행관리>리스트(BO065001){비회원 + B2B / B2C} - ( hiuxc 작업 )",
+    },
 ];
